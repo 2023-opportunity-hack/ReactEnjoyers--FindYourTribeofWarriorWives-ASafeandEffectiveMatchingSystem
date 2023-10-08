f0a80fc3adea6af2462fa06bdeff9c56159126be
nickfarm27 <nicholasfarm27@gmail.com>
2023-10-09 00:56:07 +0800

    Add sign in and register page

---
+++ src/app/idme/callback/page.js
@@ -0,0 +1,11 @@
+export default function Callback() {
+  // get query params
+  // const router = useRouter()
+  // const { code, state } = router.query
+
+  return (
+    <>
+      Test
+    </>
+  )
+}
