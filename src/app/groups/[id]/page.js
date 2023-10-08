5259f376b72afd38308a5fe66f832f958f743ebc
Daniel Yuen <yuendaniel33@gmail.com>
2023-10-09 02:45:29 +0800

    add card

--- src/app/groups/[id]/page.js
+++ src/app/groups/[id]/page.js
@@ -18,7 +18,9 @@ import {
   ListItem,
   Tag,
   Badge,
+  Card,
 } from "@chakra-ui/react";
+import LongEventCard from "../../components/longEventCard";
 import { MdLocalShipping } from "react-icons/md";
 import { events } from "../../../../content";
 
@@ -51,114 +53,117 @@ const data = {
 export default function EventPage({ params }) {
   return (
     <Container maxW={"7xl"}>
-      <SimpleGrid
-        columns={{ base: 1, lg: 2 }}
-        spacing={{ base: 8, md: 10 }}
-        py={{ base: 18, md: 19 }}
-      >
-        <Flex>
-          <Image
-            rounded={"md"}
-            alt={"product image"}
-            src={
-              data.bg
-            }
-            fit={"cover"}
-            align={"center"}
-            w={"100%"}
-            h={{ base: "100%", sm: "400px", lg: "550px" }}
-          />
-        </Flex>
-        <Stack spacing={{ base: 6, md: 10 }}>
-          <Box as={"header"}>
-            {data.online && (
-              <Tag colorScheme='blue' marginRight='1rem'>
-                Online Only
-              </Tag>
-            )}
-            <Tag colorScheme='orange'>{data.name}</Tag>
-            <Heading
-              lineHeight={1.1}
-              fontWeight={600}
-              fontSize={{ base: "2xl", sm: "4xl", lg: "5xl" }}
-            >
-              {`${data.name} Warrior Wives of ${data.location}`}
-            </Heading>
-            <Text
-              color={useColorModeValue("gray.900", "gray.400")}
-              fontWeight={300}
-              fontSize={"2xl"}
-            >
-              {data.location}
-            </Text>
-          </Box>
-
-          <Stack
-            spacing={{ base: 4, sm: 6 }}
-            direction={"column"}
-            divider={
-              <StackDivider
-                borderColor={useColorModeValue("gray.200", "gray.600")}
-              />
-            }
-          >
-            <VStack spacing={{ base: 4, sm: 6 }}>
-              <Text
-                color={useColorModeValue("gray.500", "gray.400")}
-                fontSize={"2xl"}
-                fontWeight={"300"}
+      <Card paddingY='1rem' paddingX='2rem' mb='3rem'>
+        <SimpleGrid
+          columns={{ base: 1, lg: 2 }}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 18, md: 19 }}
+        >
+          <Flex>
+            <Image
+              rounded={"md"}
+              alt={"product image"}
+              src={data.bg}
+              fit={"cover"}
+              align={"center"}
+              w={"100%"}
+              h={{ base: "100%", sm: "400px", lg: "520px" }}
+            />
+          </Flex>
+          <Stack spacing={{ base: 6 }}>
+            <Box as={"header"}>
+              <Heading
+                lineHeight={1.1}
+                fontWeight={700}
+                fontSize={{ base: "2xl", sm: "4xl", lg: "5xl" }}
+                mb="1rem"
               >
-                {data.description}
-              </Text>
-              {/* <Text fontSize={"lg"}>{data.longDescription}</Text> */}
-            </VStack>
-            <Box>
-              <Text
-                fontSize={{ base: "16px", lg: "18px" }}
-                color='orange'
-                fontWeight={"500"}
-                textTransform={"uppercase"}
-                mb={"4"}
-              >
-                Details
-              </Text>
-
-              <SimpleGrid columns={{ base: 1, md: 1 }} spacing={10}>
-                <List spacing={2}>
-                  <ListItem>
-                    <span className='font-bold'>Location: </span>
-                    Zoom
-                  </ListItem>
-                  <ListItem>
-                    <span className='font-bold'>Interest: </span>{" "}
-                    {data.tags.map((t) => (
-                      <Badge colorScheme={t.color} marginRight='0.5rem'>
-                        {t.name}
-                      </Badge>
-                    ))}
-                  </ListItem>
-                </List>
-              </SimpleGrid>
-
-              <Button
-                rounded={"none"}
-                w={"full"}
-                mt={8}
-                size={"lg"}
-                py={"7"}
-                bg={useColorModeValue("gray.900", "gray.50")}
-                color={useColorModeValue("white", "gray.900")}
-                textTransform={"uppercase"}
-                _hover={{
-                  boxShadow: "lg",
-                }}
-              >
-                Join Group
-              </Button>
+                {`${data.name} Warrior Wives of ${data.location}`}
+              </Heading>
+              {data.online && (
+                <Tag colorScheme='blue' marginRight='1rem'>
+                  Online Only
+                </Tag>
+              )}
+              <Tag colorScheme='orange'>{data.name}</Tag>
             </Box>
+
+            <Stack
+              spacing={{ base: 4, sm: 6 }}
+              direction={"column"}
+              divider={
+                <StackDivider
+                  borderColor={useColorModeValue("gray.200", "gray.600")}
+                />
+              }
+            >
+              <VStack spacing={{ base: 4, sm: 6 }}>
+                <Text
+                  color={useColorModeValue("gray.500", "gray.400")}
+                  fontSize={"2xl"}
+                  fontWeight={"300"}
+                >
+                  {data.description}
+                </Text>
+                {/* <Text fontSize={"lg"}>{data.longDescription}</Text> */}
+              </VStack>
+              <Box>
+                <Text
+                  fontSize={{ base: "16px", lg: "18px" }}
+                  color='orange'
+                  fontWeight={"500"}
+                  textTransform={"uppercase"}
+                  mb={"4"}
+                >
+                  Details
+                </Text>
+
+                <SimpleGrid columns={{ base: 1, md: 1 }} spacing={10}>
+                  <List spacing={2}>
+                    <ListItem>
+                      <span className='font-bold'>Location: </span>
+                      Zoom
+                    </ListItem>
+                    <ListItem>
+                      <span className='font-bold'>Interest: </span>{" "}
+                      {data.tags.map((t) => (
+                        <Badge colorScheme={t.color} marginRight='0.5rem'>
+                          {t.name}
+                        </Badge>
+                      ))}
+                    </ListItem>
+                  </List>
+                </SimpleGrid>
+
+                <Button
+                  rounded={"none"}
+                  w={"full"}
+                  mt={8}
+                  size={"lg"}
+                  py={"7"}
+                  bg={useColorModeValue("gray.900", "gray.50")}
+                  color={useColorModeValue("white", "gray.900")}
+                  textTransform={"uppercase"}
+                  _hover={{
+                    boxShadow: "lg",
+                  }}
+                >
+                  Join Group
+                </Button>
+              </Box>
+            </Stack>
           </Stack>
-        </Stack>
-      </SimpleGrid>
+        </SimpleGrid>
+      </Card>
+      <div className='max-w-[1200px] m-auto'>
+        <Text heading fontSize={"2rem"} color='gray.800' pb='5'>
+          Events
+        </Text>
+
+        {events.map((event) => {
+          return <LongEventCard key={event.id} event={event} />;
+        })}
+      </div>
     </Container>
   );
 }
