49eced9db6ecd7b514839c0ecc0e9aba245faf30
ZhengJieGan <ganzj45@gmail.com>
2023-10-08 13:42:21 +0800

    Skeleton

---
+++ src/app/events/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+import EventCard from "../components/eventCard";
+
+export default function Events() {
+  return (
+    <div>
+      <EventCard id="testing" />
+    </div>
+  );
+}
